7e9036d22d15cc91fb070952debf960781c88609
VE00YM347 <souravk@yamaha-motor-india.com>
2023-06-03 00:57:44 +0530

    understanding parcel bundler, package.json and package-lock.json

---
+++ Day-2/App.js
@@ -0,0 +1,41 @@
+import React from "react";
+import ReactDOM  from "react-dom/client";
+
+{
+    /* <div id='parent'>
+      <div id='child'>
+          <h1> I'm h1 tag</h1>
+      </div>
+  </div> */
+  }
+  
+  const parent = React.createElement(
+    "div",
+    { id: "parent" },
+    React.createElement(
+      "div",
+      { id: "child" },
+      [ 
+        React.createElement("h1", {}, "I'm an h1 tag"),
+        React.createElement("h2", {}, "I'm an h2 tag")
+      ]
+    )
+  );
+  
+  // It will return the javascript object
+  console.log(parent)
+  
+  // ReactElement(object) => HTML (Browser Understands)
+  
+  // const heading = React.createElement(
+  //   "h1",
+  //   { id: "heading" },
+  //   "hello world from react"
+  // );
+  
+  console.log(parent)
+  
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  
+  // This will convert js object to h1 tag
+  root.render(parent);
