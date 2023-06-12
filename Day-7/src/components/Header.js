233da9517e8c586a3104dc5d9c8840318b2a129f
VE00YM347 <souravk@yamaha-motor-india.com>
2023-06-12 19:38:24 +0530

    we have used shimmer ui concept, conditional rendering, search restaurant

---
+++ Day-7/src/components/Header.js
@@ -0,0 +1,41 @@
+import { LOGO_URL } from "../utils/constants";
+import { useState } from "react";
+
+const Header = () => {
+  const [btnNameReact, setBtnNameReact] = useState("Login");
+
+  // if no dependency array => then useEffect will be called on every component render
+  // if dependency array is empty => then useEffect will be called only in the initial render(just once)
+  // if dependency array is btnNameReact => then useEffect will be called everytime btnNameReact is updated.
+  useEffect(()=>{
+    console.log("useEffect called")
+  }, [btnNameReact])
+  
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src={LOGO_URL}></img>
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+          <button
+            className="login"
+            onClick={() => {
+              btnNameReact == "Login"
+                ? setBtnNameReact("Logout")
+                : setBtnNameReact("Login");
+            }}
+          >
+            {btnNameReact}
+          </button>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
